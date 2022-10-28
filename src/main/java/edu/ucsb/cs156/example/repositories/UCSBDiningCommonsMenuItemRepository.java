8a6a90d662c72668a4bd2153c0c8eabd1804139d
Vyom Singh <vyomsaritsingh@ucsb.edu>
2022-10-27 17:38:17 -0700

    CrudRepository<UCSBDiningCommonsMenuItem, String> fixed to have Long instead of string

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBDiningCommonsMenuItemRepository extends CrudRepository<UCSBDiningCommonsMenuItem, String> {
+public interface UCSBDiningCommonsMenuItemRepository extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {
 }
